f1e412ab2b4b091ad10a7c5d2bb480929b8ef093
Balaji Varadarajan <varadarb@uber.com>
2018-07-15 22:28:51 -0700

    Fix lint issue

--- hoodie-client/src/main/java/com/uber/hoodie/table/HoodieMergeOnReadTable.java
+++ hoodie-client/src/main/java/com/uber/hoodie/table/HoodieMergeOnReadTable.java
@@ -37,7 +37,6 @@ import com.uber.hoodie.common.table.log.block.HoodieLogBlock.HeaderMetadataType;
 import com.uber.hoodie.common.table.timeline.HoodieActiveTimeline;
 import com.uber.hoodie.common.table.timeline.HoodieInstant;
 import com.uber.hoodie.common.table.view.HoodieTableFileSystemView;
-import com.uber.hoodie.common.util.CompactionUtils;
 import com.uber.hoodie.common.util.FSUtils;
 import com.uber.hoodie.config.HoodieWriteConfig;
 import com.uber.hoodie.exception.HoodieCompactionException;
